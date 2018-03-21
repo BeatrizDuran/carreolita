1c364a9db527b565ab34387fe23d10da36b9a421
tic-rt16311016@uthermosillo.edu.mx <beatrizkrn@hotmail.com>
2018-03-20 19:23:01 -0700

    el cambio

---
+++ Carreolita/AccionesCarreola.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carreolita
+{
+    public class AccionesCarreola
+    {
+        bool estadoCarreolaAbierta = true;
+        bool estadoCarreolaCerrada = true;
+        bool estadoPasear = true;
+        bool estadoFrenar = true;
+        bool estadoFreno = true;
+        public void CarreolaAbierta()
+        {
+            if (estadoCarreolaAbierta == true)
+            {
+                Console.WriteLine("Carreola abierta");
+                estadoCarreolaAbierta = false;
+            }
+            else
+            {
+                Console.WriteLine("La carreola esta abierta");
+                estadoCarreolaAbierta = true;
+            }
+        }
+        public void CarreolaCerrada()
+        {
+            if (estadoCarreolaCerrada == true)
+            {
+                Console.WriteLine("Carreola cerrada");
+                estadoCarreolaCerrada = false;
+            }
+            else if (estadoCarreolaCerrada == false)
+                Console.WriteLine("La carreola esta cerrada");
+        }
+        public void PasearBendicion()
+        {
+            if (estadoCarreolaCerrada == false && estadoPasear == true)
+            {
+                Console.WriteLine("La bendición no se puede pasear, la carreola esta cerrada");
+                estadoPasear = false;
+            }
+            else 
+            {
+                Console.WriteLine("La bendicion se está paseando");
+                estadoPasear = false;
+            }
+        }
+        public void FrenarCarreola()
+        {
+            if (estadoPasear == true && estadoFrenar == true)
+            {
+                Console.WriteLine("El freno está puesto, no se puede pasear la bendición");
+                estadoFrenar = false;
+            }
+            else
+            {
+                Console.WriteLine("La carreola no esta frenada");
+                estadoFrenar = true;
+            }
+        }
+        public void QuitarFreno()
+        {
+            if (estadoFreno == true)
+            {
+                Console.WriteLine("El freno esta aplicado");
+                estadoFreno = false;
+            }
+            else
+            {
+                Console.WriteLine("El freno se ha retirado");
+                estadoFreno = true;
+            }
+        }
+    }
+}
--- Carreolita/Program.cs
+++ Carreolita/Program.cs
@@ -10,6 +10,54 @@ namespace Carreolita
     {
         static void Main(string[] args)
         {
+            int opcion;
+            int valor;
+            AccionesCarreola acciones = new AccionesCarreola();
+            try
+            {
+                Console.WriteLine("-------------------MENU DE CARREOLA----------------");
+                Console.WriteLine("     1. Carreola abierta");
+                Console.WriteLine("     2. Carreola cerrada");
+                Console.WriteLine("     3. Pasear bendición");
+                Console.WriteLine("     4. Frenar carreola");
+                Console.WriteLine("     5. Quitar freno");
+                Console.WriteLine("     6. Salir");
+                Console.WriteLine("     Ingrese alguna opción:");
+                do
+            {
+                    valor = int.Parse(Console.ReadLine());
+                    switch (valor)
+                    {
+                        case 1:
+                            acciones.CarreolaAbierta();
+                            break;
+                        case 2:
+                            acciones.CarreolaCerrada();
+                            break;
+                        case 3:
+                            acciones.PasearBendicion();
+                            break;
+                        case 4:
+                            acciones.FrenarCarreola();
+                            break;
+                        case 5:
+                            acciones.QuitarFreno();
+                            break;
+                        case 6:
+                            break;
+                    }
+            } while (valor != 7);
+            }
+            //Mandar el mensaje de error
+            catch (FormatException er)
+            {
+                Console.WriteLine(er.ToString());
+            }
+            catch(StackOverflowException error)
+            {
+                Console.WriteLine(error.ToString());
+            }
+            Console.ReadKey();
         }
     }
 }
